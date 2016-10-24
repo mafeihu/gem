071bd070787fd70bdcbf104666bcd2ed872545c2
mafeihu <110901542@qq.com>
2016-10-24 15:44:02 +0800

    seedbank运行成功

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  telphone   :string
+#  age        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Member < ApplicationRecord
 end
---
+++ db/seeds/development/members.seeds.rb
@@ -0,0 +1,2 @@
+puts "添加字段"
+Member.create(:name => "马飞虎", :telphone => "14797478668", :age => "25")
